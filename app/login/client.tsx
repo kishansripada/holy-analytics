2d6e41a0c8c0cf19a782dbdfa3fc79bdf07c7719
kishansripada <kishansripada@gmail.com>
2024-02-18 20:58:11 -0500

    make it a client component

---
+++ app/login/client.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import Link from "next/link";
+import { headers, cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import { AppWrapper } from "@/components/ui/app-wrapper";
+import { VStack } from "@/components/ui/stacks";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { createClient } from "@/utils/supabase/client";
+import { useState } from "react";
+
+export default function Client({ searchParams }: { searchParams: { message: string } }) {
+   const supabase = createClient();
+   const [email, setEmail] = useState("");
+   const signIn = async () => {
+      const { data, error } = await supabase.auth.signInWithOtp({
+         email,
+         options: {
+            emailRedirectTo: `${window.location.origin}/auth/callback`,
+         },
+      });
+   };
+   // const [isUsingEmail, setIsUsingEmail] = useState(false);
+
+   return (
+      <AppWrapper
+         className={"flex flex-col items-center justify-center h-screen w-full "}
+         style={{
+            // backgroundImage: "linear-gradient(180deg, #3A2C34 0%, #22191E 51.04%, #231A1F 97.92%)",
+            background: "linear-gradient(180deg, rgba(255, 0, 122, 0.15) 0%, rgba(188, 0, 0, 0.38) 100%)",
+         }}
+      >
+         <p>
+            {" "}
+            {`${process.env.NEXT_PUBLIC_VERCEL_URL ? "https" : "http"}://${process.env.NEXT_PUBLIC_VERCEL_URL || "localhost:3002"}/auth/callback`}
+         </p>
+         <VStack className="max-w-[330px] w-[330px] items-center gap-6">
+            <Link href={"/"}>
+               <img className="w-8" src="/logo.png" alt="" />
+            </Link>
+            <p className="text-xl w-full  font-semibold text-center">Sign in to holy user</p>
+
+            <VStack className="w-full gap-3">
+               <Button className="">
+                  <p>Continue with Google</p>
+               </Button>
+               {/* {isUsingEmail && (
+                  <>
+                     <HDivider></HDivider>
+                     <Input
+                        value={email}
+                        onChange={(e) => {
+                           setEmail(e.target.value);
+                        }}
+                        placeholder="Enter your email address..."
+                        className="bg-transparent text-white border dark:border-neutral-700"
+                     ></Input>
+                  </>
+               )} */}
+
+               <Button
+                  // onClick={() => {
+                  //    if (isUsingEmail) {
+                  //       signInWithEmail();
+                  //    } else {
+                  //       setIsUsingEmail(true);
+                  //    }
+                  // }}
+                  className=""
+               >
+                  <p>Continue with GitHub</p>
+               </Button>
+            </VStack>
+            <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
+               <form className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground" action={signIn}>
+                  <label className="text-md" htmlFor="email">
+                     Email
+                  </label>
+                  <input
+                     onChange={(event) => {
+                        setEmail(event.target.value);
+                     }}
+                     value={email}
+                     className="rounded-md px-4 py-2 bg-inherit border mb-6"
+                     name="email"
+                     placeholder="you@example.com"
+                     required
+                  />
+
+                  <button className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2">Sign In</button>
+               </form>
+            </div>
+         </VStack>
+      </AppWrapper>
+   );
+}
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,96 +1,21 @@
-import Link from "next/link";
-import { headers, cookies } from "next/headers";
-import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
-import { AppWrapper } from "@/components/ui/app-wrapper";
-import { VStack } from "@/components/ui/stacks";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-
-export default async function Login({ searchParams }: { searchParams: { message: string } }) {
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
+import Client from "./client";
+export const dynamic = "force-dynamic";
+const getServerSideProps = async () => {
    const cookieStore = cookies();
    const supabase = createClient(cookieStore);
+
    const {
       data: { user },
    } = await supabase.auth.getUser();
+
    if (user) {
       redirect("/dashboard");
    }
-   const signIn = async (formData: FormData) => {
-      "use server";
-
-      const email = formData.get("email") as string;
-
-      const cookieStore = cookies();
-      const supabase = createClient(cookieStore);
-
-      const { data, error } = await supabase.auth.signInWithOtp({
-         email,
-         options: {
-            emailRedirectTo: `${process.env.NEXT_PUBLIC_VERCEL_URL ? "https" : "http"}://${
-               process.env.NEXT_PUBLIC_VERCEL_URL || "localhost:3002"
-            }/auth/callback`,
-         },
-      });
-   };
-   // const [isUsingEmail, setIsUsingEmail] = useState(false);
-
-   return (
-      <AppWrapper
-         className={"flex flex-col items-center justify-center h-screen w-full "}
-         style={{
-            // backgroundImage: "linear-gradient(180deg, #3A2C34 0%, #22191E 51.04%, #231A1F 97.92%)",
-            background: "linear-gradient(180deg, rgba(255, 0, 122, 0.15) 0%, rgba(188, 0, 0, 0.38) 100%)",
-         }}
-      >
-         <VStack className="max-w-[330px] w-[330px] items-center gap-6">
-            <Link href={"/"}>
-               <img className="w-8" src="/logo.png" alt="" />
-            </Link>
-            <p className="text-xl w-full  font-semibold text-center">Sign in to holy user</p>
-
-            <VStack className="w-full gap-3">
-               <Button className="">
-                  <p>Continue with Google</p>
-               </Button>
-               {/* {isUsingEmail && (
-                  <>
-                     <HDivider></HDivider>
-                     <Input
-                        value={email}
-                        onChange={(e) => {
-                           setEmail(e.target.value);
-                        }}
-                        placeholder="Enter your email address..."
-                        className="bg-transparent text-white border dark:border-neutral-700"
-                     ></Input>
-                  </>
-               )} */}
-
-               <Button
-                  // onClick={() => {
-                  //    if (isUsingEmail) {
-                  //       signInWithEmail();
-                  //    } else {
-                  //       setIsUsingEmail(true);
-                  //    }
-                  // }}
-                  className=""
-               >
-                  <p>Continue with GitHub</p>
-               </Button>
-            </VStack>
-            <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
-               <form className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground" action={signIn}>
-                  <label className="text-md" htmlFor="email">
-                     Email
-                  </label>
-                  <input className="rounded-md px-4 py-2 bg-inherit border mb-6" name="email" placeholder="you@example.com" required />
-
-                  <button className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2">Sign In</button>
-               </form>
-            </div>
-         </VStack>
-      </AppWrapper>
-   );
+};
+export default async function Page({}) {
+   const _ = await getServerSideProps();
+   return <Client></Client>;
 }
